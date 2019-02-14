49298eec2050867e39eed3787a22ad9acc63fa9f
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-02-14 17:16:33 -0300

    Tratando exceḉões no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -22,10 +22,43 @@ namespace xadrez_console.tabuleiro
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.Linha, pos.Coluna] = p;       // a string da peça p é colocada na posiçao[linha,coluna]
             p.posicao = pos;                        // é definida a posição da peça p
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= linhas || pos.Coluna < 0 || pos.Coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição invalida");
+            }
         }
     }
 }
